af7de8fc5d0aa721c34d763c1146d76a2b2125cc
supperik <mr.niki234@mail.ru>
2024-11-10 18:11:53 +0300

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+AUTHOR = 'Nikita Popov'
+SITENAME = 'ovoc_cv_pelican'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'W-SU'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
